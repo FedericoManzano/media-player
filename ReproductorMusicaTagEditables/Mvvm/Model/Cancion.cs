6c307055f5f0967e7b6d97ed133e349f4677e28e
feder <federicomanzano111@gmail.com>
2023-10-25 09:45:03 -0300

    Métodos de la clase object implementados en el modelo Model/Cancion ToString, Equals, Clone

--- ReproductorMusicaTagEditables/Mvvm/Model/Cancion.cs
+++ ReproductorMusicaTagEditables/Mvvm/Model/Cancion.cs
@@ -1,12 +1,120 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ReproductorMusicaTagEditables.Mvvm.Model
 {
     public class Cancion
     {
+        public string Numero { get; set; } = string.Empty;
+        public string Titulo { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+        public string Album { get; set; } = string.Empty;
+        public string Genero { get; set;} = string.Empty;
+        public string Path { get; set; } = string.Empty;
+        public string Duracion { get; set; } = string.Empty;
+        public string FechaLanzamiento { get; set;} = string.Empty;
+
+
+
+        Cancion()
+        {
+            Numero = string.Empty;
+            Titulo = string.Empty;
+            Artista = string.Empty;
+            Album = string.Empty;
+            Path = string.Empty;
+            Duracion = string.Empty;
+            FechaLanzamiento = string.Empty;
+        }
+
+
+        public Cancion CrearCancion(string path)
+        {
+            if(System.IO.File.Exists(path))
+            {
+                var tag = TagLib.File.Create(path);
+                return new Cancion
+                {
+                    Path = path,
+                    Numero = ExtraerNumero(tag),
+                    Titulo = ExtraerTitulo(tag, path),
+                    Artista = ExtraerArtista(tag),
+                    Genero = ExtraerGenero(tag),
+                    Album = ExtraerAlbum(tag),
+                    Duracion = ExtraerDuracion(tag),
+                    FechaLanzamiento = ExtraerFechaLanzamiento(tag),
+                };
+            }
+            return null;
+        }
+
+        private string ExtraerNumero(TagLib.File tag)
+        {
+            string num = tag.Tag.Track > 0 ? tag.Tag.Track.ToString() : string.Empty;
+            return num;
+        }
+        
+        private string ExtraerTitulo(TagLib.File tag, string path)
+        {
+
+            string tit = tag.Tag.Title ?? string.Empty;
+            if(tit == string.Empty)
+            {
+                FileInfo fi = new FileInfo(path);
+                return System.IO.Path.GetFileNameWithoutExtension(fi.Name);
+            }
+            return tit;
+        }
+
+        private string ExtraerArtista(TagLib.File tag)
+        {
+#pragma warning disable CS0618 // El tipo o el miembro están obsoletos
+            string tit = tag.Tag.Artists != null ? tag.Tag.Artists[0]: string.Empty;
+#pragma warning restore CS0618 // El tipo o el miembro están obsoletos
+            return tit;
+        }
+
+        private string ExtraerGenero(TagLib.File tag)
+        {
+            string tit = tag.Tag.Genres != null ? tag.Tag.Genres[0] : string.Empty;
+            return tit;
+        }
+
+        private string ExtraerAlbum(TagLib.File tag)
+        {
+            string tit = tag.Tag.Album ?? string.Empty;
+            return tit;
+        }
+
+        private string ExtraerDuracion(TagLib.File tag)
+        {
+            string tit = tag.Tag.Length ?? string.Empty;
+            return tit;
+        }
+        private string ExtraerFechaLanzamiento (TagLib.File tag)
+        {
+            return tag.Tag.DateTagged?.ToString(@"dd/MM/yyyy");
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj.GetType() != typeof(Cancion)) return false;
+            if(obj == this) return true;
+            return this.Path == ((Cancion) obj).Path;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 236998955;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Numero);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Titulo);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Artista);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Album);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Genero);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Path);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(FechaLanzamiento);
+            return hashCode;
+        }
     }
 }
