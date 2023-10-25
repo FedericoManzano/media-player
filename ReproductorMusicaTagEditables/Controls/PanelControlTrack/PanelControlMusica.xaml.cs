7b7d1135ad33d92971ce29ee6d8bd318f21c82a1
feder <federicomanzano111@gmail.com>
2023-10-25 04:51:32 -0300

    PanelControlTrack Avanzado en la definición de RoutedEvent para el evento click de los botones. Se añadió un Slider para el control del volumen y se estableció la propiedad Visibility de este elemento sujeta a el botón con el ícono del parlate.

--- ReproductorMusicaTagEditables/Controls/PanelControlTrack/PanelControlMusica.xaml.cs
+++ ReproductorMusicaTagEditables/Controls/PanelControlTrack/PanelControlMusica.xaml.cs
@@ -15,14 +15,82 @@ using System.Windows.Shapes;
 
 namespace ReproductorMusicaTagEditables.Controls.PanelControlTrack
 {
-    /// <summary>
-    /// Lógica de interacción para PanelControlMusica.xaml
-    /// </summary>
+    
     public partial class PanelControlMusica : UserControl
     {
+        public bool visibilidadSliderVolumen = false;
+        
         public PanelControlMusica()
         {
             InitializeComponent();
+        }
+
+
+        #region EventoAnterior Definición
+        public static readonly RoutedEvent AnteriorEvent = 
+            EventManager.RegisterRoutedEvent("AnteriorClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(PanelControlMusica));  
+
+
+        public event RoutedEventHandler AnteriorClick
+        {
+            add => AddHandler(AnteriorEvent,value);
+            remove => RemoveHandler(AnteriorEvent,value);
+        }
+
+        public void OnClickAnterior (object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(AnteriorEvent));
+        }
+        #endregion
+
+
+        #region EventoSiguiente Definición
+        public static readonly RoutedEvent SiguienteEvent =
+           EventManager.RegisterRoutedEvent("SiguienteClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(PanelControlMusica));
+
+
+        public event RoutedEventHandler SiguienteClick
+        {
+            add => AddHandler(SiguienteEvent, value);
+            remove => RemoveHandler(SiguienteEvent, value);
+        }
+        
+
+        public void OnClickSiguiente(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(SiguienteEvent));
+        }
+        #endregion
+
+
+        #region EventoPlay Definición
+        public static readonly RoutedEvent PlayEvent =
+           EventManager.RegisterRoutedEvent("PlayClick", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(PanelControlMusica));
+        public event RoutedEventHandler PlayClick
+        {
+            add => AddHandler(PlayEvent, value);
+            remove => RemoveHandler(PlayEvent, value);
+        }
+
+        public void OnClickPlay(object sender, RoutedEventArgs e)
+        {
+            RaiseEvent(new RoutedEventArgs(PlayEvent));
+        }
+        #endregion
+
+
+        private void Mostrar_Slider_Volumen(object sender, RoutedEventArgs e)
+        {
+            if(contenedorVolumen.Visibility == Visibility.Visible)
+            {
+                contenedorVolumen.Visibility = Visibility.Collapsed;
+                btnVolumen.Foreground = Brushes.White;
+            }
+            else
+            {
+                contenedorVolumen.Visibility = Visibility.Visible;
+                btnVolumen.Foreground = Brushes.Orange;
+            }
         }
     }
 }
--- ReproductorMusicaTagEditables/Controls/TiempoTrack/TiempoTrack.xaml.cs
+++ ReproductorMusicaTagEditables/Controls/TiempoTrack/TiempoTrack.xaml.cs
@@ -1,28 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ReproductorMusicaTagEditables.Controls.TiempoTrack
 {
-    /// <summary>
-    /// Lógica de interacción para TiempoTrack.xaml
-    /// </summary>
     public partial class TiempoTrack : UserControl
     {
         public TiempoTrack()
         {
             InitializeComponent();
+        }
+
+        public static DependencyProperty TiempoTranscurridoProperty =
+            DependencyProperty.Register("TiempoTranscurrido", typeof(string), typeof(TiempoTrack), new PropertyMetadata("00:00:00"));
+ 
+        public string TiempoTranscurrido
+        {
+            get => GetValue(TiempoTranscurridoProperty) as string; 
+            set => SetValue(TiempoTranscurridoProperty, value);
+        }
+
+        public static DependencyProperty TiempoFaltanteProperty =
+            DependencyProperty.Register("TiempoFaltante", typeof(string), typeof(TiempoTrack), new PropertyMetadata("00:00:00"));
+
+        public string TiempoFaltante
+        {
+            get => GetValue(TiempoFaltanteProperty) as string;
+            set => SetValue(TiempoFaltanteProperty, value);
         }
     }
 }
